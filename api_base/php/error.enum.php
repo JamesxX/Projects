8e3c513f1a1e27c94ceae8d6df99ef23df50ee45
James Swift <james.rt.swift@gmail.com>
2015-06-15 17:21:30 +0100

    Fancy-ify

--- api_base/php/error.enum.php
+++ api_base/php/error.enum.php
@@ -1,10 +1,10 @@
 <?php
 
 abstract class API_ErrorEnum{
-	const Success				= -1;
+	const Success			= -1;
 	
-    const NoMethodExists		= 0;
-    const MethodFailedChecks	= 1;
+	const NoMethodExists		= 0;
+	const MethodFailedChecks	= 1;
 	const MethodAlreadyExists	= 2;
 }
 
